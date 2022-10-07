977c926de2e79172f915eaa5a4b236793981468b
Devi Kiran <devikiran333007@gmail.com>
2022-10-07 18:25:20 +0530

    code commented

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,32 +1,32 @@
-import React,{useState,useEffect} from 'react'
+// import React,{useState,useEffect} from 'react'
 
-function TodoApp() {
-    const [value,setValue] = useState('')
-    const [todos,setTodos] = useState([]);
+// function TodoApp() {
+//     const [value,setValue] = useState('')
+//     const [todos,setTodos] = useState([]);
 
-    useEffect(() => {
-        console.log('useEffect');
-    },[todos])
+//     useEffect(() => {
+//         console.log('useEffect');
+//     },[todos])
 
-    function addTodo() {
-        setTodos(prevTodos => {
-            return [...prevTodos,value];
-        });
-        setValue('');
-    }
+//     function addTodo() {
+//         setTodos(prevTodos => {
+//             return [...prevTodos,value];
+//         });
+//         setValue('');
+//     }
 
-    return (
-        <div>
-            <h1>todo app</h1>
-            <input value={value} onChange={e => setValue(e.target.value)}/>
-            <button onClick={addTodo}>add task</button>
-            <ul>
-                {todos.map(todo => {
-                    return <li>{todo}</li>
-                })}
-            </ul>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <h1>todo app</h1>
+//             <input value={value} onChange={e => setValue(e.target.value)}/>
+//             <button onClick={addTodo}>add task</button>
+//             <ul>
+//                 {todos.map(todo => {
+//                     return <li>{todo}</li>
+//                 })}
+//             </ul>
+//         </div>
+//     )
+// }
 
-export default TodoApp;
+// export default TodoApp;
